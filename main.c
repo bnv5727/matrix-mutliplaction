9e6bf6ed2a4a826625767f230a39e6b9ee1ac2cf
bnv5727 <bnv5727@gmail.com>
2025-11-24 10:50:45 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,37 +1,26 @@
 #include<stdio.h>
 int main()
 {
- int a[10][10],b[10][10],c[10][10],i,j,k;
+ int a[10][10],t[10][10],i,j,r,c;
+printf("enter rows and columns");
+ scanf("%d%d",&r,&c);
  printf("Enter elements into A matrix");
- for(i=0;i<2;i++)
- {  for(j=0;j<2;j++)
+ for(i=0;i<r;i++)
+ {  for(j=0;j<c;j++)
   {   scanf("%d",&a[i][j]);
   } 
 }
- printf("Enter elements into B matrix");
- for(i=0;i<2;i++)
- {  for(j=0;j<2;j++)
-  {   scanf("%d",&b[i][j]);
-  } 
+for(i=0;i<r;i++)
+ {  for(j=0;j<c;j++)
+ {
+ 	t[j][i]=a[i][j];
+ } }
+printf("transpose matrix is \n");
+for(i=0;i<c;i++)
+ {  for(j=0;j<r;j++)
+ {
+ 	printf("%d\t",t[i][j]);
+ }
+ printf("\n");
 }
-printf("matrix multiplication is\n");
-for(i=0;i<2;i++)
- {
-  for(j=0;j<2;j++)
-  {
-   c[i][j]=0;
-   for(k=0;k<2;k++)
-      {
-   c[i][j]=c[i][j]+a[i][k]*b[k][j];
-} 
- }
- }
-for(i=0;i<2;i++)
- {
-  for(j=0;j<2;j++)
-  {
-   printf(" %d\t",c[i][j]);
-  } printf("\n");
-   }
-return 0; 
 }
